deb478ffa976586491b69f6c6ea17b24973e491e
TheCuddlyBear <basvthazeveld@outlook.com>
2023-05-11 14:50:43 +0200

    Created model class with init

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
-import CorpusReader
+import re
 
 class Model:
-    def __init__(self, tokens):
+    def __init__(self, tokens: list):
+        self.tokens =  [i.lower() for i in tokens if not re.match('\W', i)]
+        for i in range(len(self.tokens)): # loop through every sentence
+            p = self.tokens[i] # create temp variable
+            s = "<s>" + p + "</s>" # add the boundaries
+            self.tokens[i] = s # put it back in the list
