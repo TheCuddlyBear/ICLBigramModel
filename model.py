f396465713ae7228cef5b3741634011de7d2313f
Bas van 't Hazeveld <basvthazeveld@outlook.com>
2023-05-16 17:02:12 +0200

    Added check if tokens is none, Added template for rest of the model, Renamed variables and functions to match pep8

--- model.py
+++ model.py
@@ -6,32 +6,44 @@ from tqdm import tqdm
 
 class BigramModel:
     def __init__(self, tokens: list):
-        self.tokens: list = self.addSentenceBoundaries(tokens)
-        bigramCounts = self.countBigrams(self.tokens)
-        bigramCountTuples = bigramCounts.most_common(len(bigramCounts))
-        self.frequencyTable: pd.DataFrame = pd.DataFrame(bigramCountTuples, columns=['bigram', 'count'])
+        if (tokens == None):
+            print("Tokens cannot be null!")
+        else:
+            self.tokens: list = self.__add_sentence_boundaries(tokens)
+            _bigram_counts = self.__make_count_bigrams(self.tokens)
+            _bigram_count_tuples = _bigram_counts.most_common(len(_bigram_counts))
+            self.frequencyTable: pd.DataFrame = pd.DataFrame(_bigram_count_tuples, columns=['bigram', 'count'])
 
-    def countBigrams(self, tokens: list) -> Counter:
-        bigramCounts = Counter()
+    def probability(self, w: str, w_n: str, smooth_constant: float = 0.0) -> float:
+        pass
+
+    def perplexity(self, sent: list, smoothing_constant: float = 0.0) -> float:
+        pass
+
+    def choose_successor(self, word: str, smooth_constant: float = 0.0) -> str:
+        pass
+
+    def __make_count_bigrams(self, tokens: list) -> Counter:
+        bigram_counts = Counter()
         for p, words in enumerate(tqdm(tokens, ncols=100, desc='Making and counting Bigrams')):
             bigrams: list = []
             for i in range(len(words) - 1):
                 bigrams.append(words[i] + " " + words[i + 1])
-            bigramCounts += Counter(bigrams)
-        return bigramCounts
+            bigram_counts += Counter(bigrams)
+        return bigram_counts
 
-    def addSentenceBoundaries(self, tokens: list) -> list:
-        tokensWithoutPunctuation = self.removePunctuationTokens(tokens)
-        tokensWithBoundaries: list = []
+    def __add_sentence_boundaries(self, tokens: list) -> list:
+        tokens_without_punctuation = self.__remove_punctuation_tokens(tokens)
+        tokens_with_boundaries: list = []
         item: list
-        for i, item in enumerate(tqdm(tokensWithoutPunctuation, ncols=100, desc='Adding boundaries')):
+        for i, item in enumerate(tqdm(tokens_without_punctuation, ncols=100, desc='Adding boundaries')):
             item.insert(0, "<s>")
             item.append("</s>")
-            tokensWithBoundaries.append(item)
-        return tokensWithBoundaries
+            tokens_with_boundaries.append(item)
+        return tokens_with_boundaries
 
-    def removePunctuationTokens(self, tokens: list):
-        toReturn: list = []
+    def __remove_punctuation_tokens(self, tokens: list):
+        to_return: list = []
         for sent in tokens:
-            toReturn.append([p.lower() for p in sent if not re.match('\W', p)])
-        return toReturn
+            to_return.append([p.lower() for p in sent if not re.match('\W', p)])
+        return to_return
