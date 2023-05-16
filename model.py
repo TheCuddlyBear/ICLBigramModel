b09b7f8c5fd7190f122272a6a38d9ad486cb24e9
Bas van 't Hazeveld <basvthazeveld@outlook.com>
2023-05-16 21:09:14 +0200

    Added probability function

--- model.py
+++ model.py
@@ -9,48 +9,94 @@ class BigramModel:
         if (tokens == None):
             print("Tokens cannot be null!")
         else:
-            self.tokens: list = self._add_sentence_boundaries(tokens)
-            _bigram_counts = self._make_count_bigrams(self.tokens)
+            self.tokens: list = BigramModel.add_sentence_boundaries(tokens)
+            _unigram_counts = BigramModel.make_count_unigrams(self.tokens)
+            _unigram_counts_tuples = _unigram_counts.most_common(len(_unigram_counts))
+            self.unigram_frequency_table = pd.DataFrame(_unigram_counts_tuples, columns=['unigram', 'count'])
+            self.unigram_frequency_table.drop(
+                self.unigram_frequency_table[self.unigram_frequency_table['unigram'] == '</s>'].index, inplace=True)
+            _bigram_counts = BigramModel.make_count_bigrams(self.tokens)
             _bigram_count_tuples = _bigram_counts.most_common(len(_bigram_counts))
-            self.frequencyTable: pd.DataFrame = pd.DataFrame(_bigram_count_tuples, columns=['bigram', 'count'])
+            self.bigram_frequency_table = pd.DataFrame(_bigram_count_tuples, columns=['bigram', 'count'])
 
-    def probability(self, w: str, w_n: str, smooth_constant: float = 0.0) -> float:
+    def probability(self, w: str, w_n: str, smoothing_constant: float = 0.0):
+        """
+        @param w: The token we have just seen
+        @param w_n: the probability of seeing token w_n
+        @param smoothing_constant: the constant with which smoothing is applied
+        This function calculates the probability of seeing token w_n after seeing token w
+        """
+        bigram: tuple = (w, w_n)
+        try:
+            bigram_count = self.bigram_frequency_table.loc[self.bigram_frequency_table['bigram'] == bigram]['count'].tolist()[0]
+        except:
+            return 0.0
+        try:
+            unigram_count = self.unigram_frequency_table.loc[self.unigram_frequency_table['unigram'] == w_n]['count'].tolist()[0]
+        except:
+            return 0.0
+        if smoothing_constant == 0.0:
+            # Locate the bigram or unigram we want the probability of
+            return bigram_count / unigram_count
+        else:
+            total_words = len(self.unigram_frequency_table)
+            t = bigram_count + smoothing_constant
+            n = unigram_count + smoothing_constant * total_words
+            return t/n
+
+
+
+    def perplexity(self, sent: list, smoothing_constant: float = 1.0) -> float | None:
         pass
 
-    def perplexity(self, sent: list, smoothing_constant: float = 1.0) -> float:
+    def choose_successor(self, word: str, smoothing_constant: float = 0.0) -> str | None:
         pass
 
-    def choose_successor(self, word: str, smooth_constant: float = 0.0) -> str:
-        pass
+    @staticmethod
+    def make_count_unigrams(tokens: list) -> Counter:
+        """
+        @param tokens: list of tokenized sentences
+        Takes a list of tokenized sentences and generates the appropriate unigrams and counts them
+        """
+        unigram_counts = Counter()
+        for p, words in enumerate(
+                tqdm(tokens, ncols=100, desc='Making and counting Unigrams')):  # tqdm prints a progressbar
+            unigram_counts += Counter(words)
+        return unigram_counts
 
-    def _make_count_bigrams(self, tokens: list) -> Counter:
+    @staticmethod
+    def make_count_bigrams(tokens: list) -> Counter:
         """
         @param tokens: list of tokenized sentences
         Takes a list of tokenized sentences and generates the appropriate bigrams and counts them
         """
         bigram_counts = Counter()
-        for p, words in enumerate(tqdm(tokens, ncols=100, desc='Making and counting Bigrams')):
+        for p, words in enumerate(
+                tqdm(tokens, ncols=100, desc='Making and counting Bigrams')):  # tqdm prints a progressbar
             bigrams: list = []
             for i in range(len(words) - 1):
                 bigrams.append((words[i], words[i + 1]))
             bigram_counts += Counter(bigrams)
         return bigram_counts
 
-    def _add_sentence_boundaries(self, tokens: list) -> list:
+    @staticmethod
+    def add_sentence_boundaries(tokens: list) -> list:
         """
         @param tokens: list of tokenized sentences
         Takes a list of tokenized sentences and adds sentence boundaries to all the sentences
         """
-        tokens_without_punctuation = self._remove_punctuation_tokens(tokens)
+        tokens_without_punctuation = BigramModel.remove_punctuation_tokens(tokens)
         tokens_with_boundaries: list = []
         item: list
-        for i, item in enumerate(tqdm(tokens_without_punctuation, ncols=100, desc='Adding boundaries')):
+        for i, item in enumerate(
+                tqdm(tokens_without_punctuation, ncols=100, desc='Adding boundaries')):  # tqdm prints a progressbar
             item.insert(0, "<s>")
             item.append("</s>")
             tokens_with_boundaries.append(item)
         return tokens_with_boundaries
 
-    def _remove_punctuation_tokens(self, tokens: list):
+    @staticmethod
+    def remove_punctuation_tokens(tokens: list):
         """
         @param tokens: list of tokenized sentences
         Takes a list of tokenized sentences and removes the tokens that solemnly consist of punctuation from the list
